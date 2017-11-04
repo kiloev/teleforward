8e91d6fcacbb104c084eef9f4749c648204492b9
Kevin Putrajaya <freakperzon@yahoo.com>
2017-11-04 10:03:26 +0700

    Change origin false

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const http = require('http')
 const forward = require('http-forward')
 
 var server = http.createServer(function (req, res) {
-  req.forward = { target: process.env.REDIRECT_TARGET }
+  req.forward = {
+    target: process.env.REDIRECT_TARGET,
+    changeOrigin: true
+  }
   forward(req, res)
 })
 
