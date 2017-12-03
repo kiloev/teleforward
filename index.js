66532c12158fb12ab3dfaddf72f63b58ebecb7ab
kiloev <freakperzon@yahoo.com>
2017-12-03 20:04:35 +0700

    Deploy button

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const http = require('http')
-const forward = require('http-forward')
+/*jshint esversion: 6 */
+
+const http = require('http');
+const forward = require('http-forward');
 
 var server = http.createServer(function (req, res) {
   req.forward = {
     target: process.env.REDIRECT_TARGET,
     changeOrigin: true
-  }
-  forward(req, res)
-})
+  };
+  forward(req, res);
+});
 
-server.listen(process.env.PORT || 3000)
+server.listen(process.env.PORT || 3000);
